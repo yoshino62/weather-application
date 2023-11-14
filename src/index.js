8160887e9174ee06a5dbb4a2f7d37fbb08cf318f
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2023-11-14 16:22:59 -0500

    Injected images on JS

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ function updateWeatherData(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-weather-icon" />`;
 }
 
 function formatDate(date) {
