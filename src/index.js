1bff471019d69255fca8080e9b844af92f5ef2ca
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2023-11-14 13:37:33 -0500

    Fixed JS

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ function updateWeatherData(response) {
 }
 
 function searchCity(city) {
-  apiKey = "903bbfab25a139539824b07e1tfo211c";
-  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "903bbfab25a139539824b07e1tfo211c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateWeatherData);
 }
 
