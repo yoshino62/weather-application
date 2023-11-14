5159ccd9623ea62fb4031f58b11407725ecaff22
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2023-11-13 19:48:16 -0500

    Add the basic search engine behavior (No API)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-bar");
+  let cityElement = document.querySelector("#currrent-city");
+  cityElement.innerHTML = searchInput.value;
 
+  // call the API
+  // search for the city
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
